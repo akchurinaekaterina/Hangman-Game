227851526e747901075e2ec49ec8e3c3a0802d1b
@akchurina.es <akchurina.ekaterina@gmail.com>
2020-04-13 18:14:07 +0200

    Initial Commit

---
+++ HangmanGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HangmanGame
+//
+//  Created by Ekaterina Musiyko on 13/04/2020.
+//  Copyright © 2020 Ekaterina Akchurina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
